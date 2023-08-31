2165ccf295c68aba868d69926962a2c3ab8e7ddf
Bryan Bickel <briimcfly@Bryans-Mac-mini.local>
2023-08-31 14:57:20 -0700

    created Schema

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS company; 
+CREATE DATABASE company;
+
+CREATE TABLE department(
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role(
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE SET NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    manager_id INT,
+    role_id INT NOT NULL,
+    FOREIGN KEY(role_id) REFERENCES role(id),
+    FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE SET NULL,
+    PRIMARY KEY (id)
+);
