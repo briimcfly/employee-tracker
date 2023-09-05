6243817d11d1f5215609fa41126dacd238a41459
Bryan Bickel <briimcfly@Bryans-Mac-mini.local>
2023-09-05 15:39:02 -0700

    added initial question list

--- app/index.js
+++ app/index.js
@@ -4,8 +4,45 @@ const inquirer = require('inquirer');
 inquirer
 .prompt([
     {
-        type: 'input',
-        message: 'this is a test input',
-        name: 'test_input'
+        type: 'list',
+        message: 'WHAT WOULD YOU LIKE TO DO?: ',
+        name: 'test_input',
+        choices: [
+            {
+                value: 'view_dept',
+                name: 'view all departments'
+            },
+            {
+                value: 'view_roles',
+                name: 'view all roles'
+            },
+            {
+                value: 'view_emp',
+                name: 'view all employees'
+            },
+            {
+                value: 'add_dept',
+                name: 'add a department'
+            },
+            {
+                value: 'add_role',
+                name: 'add a role'
+            },
+            {
+                value: 'add_emp',
+                name: 'add an employee'
+            },
+            {
+                value: 'update_emp',
+                name: 'update an employee'
+            }
+        ]
     }
 ])
+.then((response) => {
+    if (response.test_input == 'add_role') {
+        console.log('that worked');
+    } else {
+        console.log('nope');
+    }
+}) 
