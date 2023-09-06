c0bdb564e4141942279b7905e0671da9a2d8ffa8
Bryan Bickel <briimcfly@Bryans-Mac-mini.local>
2023-09-05 17:55:50 -0700

    made Add Role a function called from the index.. user can add multiple roles or go back to main menu

--- app/index.js
+++ app/index.js
@@ -1,48 +1,54 @@
 const {readFile, writeFile} = require('fs');
 const inquirer = require('inquirer');
 
-inquirer
-.prompt([
-    {
-        type: 'list',
-        message: 'WHAT WOULD YOU LIKE TO DO?: ',
-        name: 'test_input',
-        choices: [
-            {
-                value: 'view_dept',
-                name: 'view all departments'
-            },
-            {
-                value: 'view_roles',
-                name: 'view all roles'
-            },
-            {
-                value: 'view_emp',
-                name: 'view all employees'
-            },
-            {
-                value: 'add_dept',
-                name: 'add a department'
-            },
-            {
-                value: 'add_role',
-                name: 'add a role'
-            },
-            {
-                value: 'add_emp',
-                name: 'add an employee'
-            },
-            {
-                value: 'update_emp',
-                name: 'update an employee'
-            }
-        ]
-    }
-])
-.then((response) => {
-    if (response.test_input == 'add_role') {
-        require('./routes/AddRole');
-    } else {
-        console.log('nope');
-    }
-}) 
+const addRole = require('./routes/AddRole');
+
+function mainMenu() {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'WHAT WOULD YOU LIKE TO DO?: ',
+            name: 'test_input',
+            choices: [
+                {
+                    value: 'view_dept',
+                    name: 'view all departments'
+                },
+                {
+                    value: 'view_roles',
+                    name: 'view all roles'
+                },
+                {
+                    value: 'view_emp',
+                    name: 'view all employees'
+                },
+                {
+                    value: 'add_dept',
+                    name: 'add a department'
+                },
+                {
+                    value: 'add_role',
+                    name: 'add a role'
+                },
+                {
+                    value: 'add_emp',
+                    name: 'add an employee'
+                },
+                {
+                    value: 'update_emp',
+                    name: 'update an employee'
+                }
+            ]
+        }
+    ])
+    .then((response) => {
+        if (response.test_input == 'add_role') {
+            addRole(mainMenu);
+        } else {
+            console.log('nope');
+        }
+    }) 
+}
+
+mainMenu();
--- app/routes/AddRole.js
+++ app/routes/AddRole.js
@@ -3,52 +3,84 @@ const {db} = require('../../server.js');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
-
-db.query('SELECT id, name FROM department', (err, departments) => {
-    if (err) {
-        console.error("Error getting results from department table: ", err.message);
-        return;
-    }
-
-    const departmentChoices = departments.map(department => ({
-        name: department.name,
-        value: department.id
-    }));
-
-    inquirer.prompt([
-    {
-        //Ask user for the name
-        type: 'input',
-        message: 'ENTER THE ROLE NAME',
-        name: 'role_name',
-    },
-    {
-        //Ask user for the name
-        type: 'input',
-        message: 'ENTER THE ROLE SALARY',
-        name: 'role_salary',
-    },
-    {
-        //Ask user for the name
-        type: 'list',
-        message: 'ENTER THE ROLE DEPARTMENT',
-        name: 'role_dept',
-        choices: departmentChoices
-    }
-])
-.then((response)=> {
-    const {role_name, role_salary } = response;
-    
-    db.query('INSERT INTO role (title, salary) VALUES (?,?)',[role_name, role_salary], (err,results)=>{
+function addRole(callback) {
+    db.query('SELECT id, name FROM department', (err, departments) => {
         if (err) {
-            console.error("Error inserting data:", err.message);
+            console.error("Error getting results from department table: ", err.message);
             return;
         }
-        console.log("row inserted", results);
-    })
+    
+        const departmentChoices = departments.map(department => ({
+            name: department.name,
+            value: department.id
+        }));
+    
+        inquirer.prompt([
+        {
+            //Ask user for the name
+            type: 'input',
+            message: 'ENTER THE ROLE NAME',
+            name: 'role_name',
+        },
+        {
+            //Ask user for the name
+            type: 'input',
+            message: 'ENTER THE ROLE SALARY',
+            name: 'role_salary',
+        },
+        {
+            //Ask user for the name
+            type: 'list',
+            message: 'ENTER THE ROLE DEPARTMENT',
+            name: 'role_dept',
+            choices: departmentChoices
+        }
+    ])
+    .then((response)=> {
+        const {role_name, role_salary, role_dept } = response;
+        
+        return new Promise((resolve, reject) => {
+            db.query('INSERT INTO role (title, salary, department_id) VALUES (?,?,?)',
+            [role_name, role_salary, role_dept],
+             (err,results)=>{
+                if (err) {
+                    reject(err);
+                    return;
+                }
+    
+                db.query('SELECT role.id, role.title, role.salary, department.name as department_name FROM role JOIN department ON role.department_id = department.id ORDER BY role.id DESC',
+                (err,roles) => {
+                    if(err) {
+                        reject(err);
+                        return;
+                    }
+    
+                    console.log(`${role_name} succesfully added! \n`);
+                    console.table(roles);
+        
+                    inquirer.prompt([
+                        {
+                            name: 'continue',
+                            type: 'confirm',
+                            message: 'WOULD YOU LIKE TO ADD ANOTHER ROLE?'
+                        }
+                    ]).then((continueResponse) => {
+                        if (continueResponse.continue) {
+                            addRole(callback);
+                        } else {
+                            callback(); 
+                        }
+                        });
+                        resolve();
+                    });
+                });
+            });
+        });
+    });
+}
 
-}) 
-})
+module.exports = addRole;
+
 
 
 
