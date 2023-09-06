2d197d317dc989cedb2e28ae8bdb561e25b14d6d
Bryan Bickel <briimcfly@Bryans-Mac-mini.local>
2023-09-06 10:43:50 -0700

    user can view all roles

--- app/routes/UpdateEmployee.js
+++ app/routes/UpdateEmployee.js
@@ -3,4 +3,4 @@ const {db} = require('../../server.js');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
-module.exports = updateEmp;
+// module.exports = updateEmp;
--- app/routes/ViewRole.js
+++ app/routes/ViewRole.js
@@ -4,8 +4,15 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
 function viewRole(callback) {
+    db.query('SELECT * from role', (err, roles) => {
+        if (err) {
+            console.error("Error getting results from Role table: ", err.message);
+            return;
+        }
+        console.table(roles);
+        callback();
+    })
 
 }
-
 
 module.exports = viewRole;
