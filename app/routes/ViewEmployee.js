ca972f7e34e1e69e1ad560d338ab5374bf3f189d
Bryan Bickel <briimcfly@gmail.com>
2023-09-06 22:23:02 -0700

    updated View All Employees to show Manager Name and Role Title instead of the ID's

--- app/routes/ViewEmployee.js
+++ app/routes/ViewEmployee.js
@@ -4,7 +4,19 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
 function viewEmp(callback) {
-    db.query('SELECT * from employee', (err, employees) => {
+    db.query(`
+    SELECT
+        employee.id,
+        CONCAT(employee.first_name, " ", employee.last_name) AS employee_name,
+        CONCAT(manager.first_name, ' ', manager.last_name) AS manager_name,
+        role.title AS role_name
+    FROM 
+        employee
+    LEFT JOIN 
+        employee AS manager ON employee.manager_id = manager.id
+    LEFT JOIN
+        role ON employee.role_id = role.id
+    `, (err, employees) => {
         if (err) {
             console.error("Error getting results from Employee table: ", err.message);
             return;
